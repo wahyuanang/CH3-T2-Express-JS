fe672b65b30597cd763cd309600c67d06456a0ae
wahyuanang <wahyuanng@students.amikom.ac.id>
2024-10-01 22:12:43 +0700

    routing menggunakan express js, postman, add request, create API in postmanlearning GET, and POST

--- server.js
+++ server.js
@@ -4,17 +4,75 @@ const express = require("express");
 const app = express();
 
 // routing
-app.get("/wahyu", (req, res) => {
+app.get("/", (req, res) => {
   res.status(200).json({
+    status: "success",
     message: "message succesfully",
   });
 });
 
+//middleware untuk membaca json dari request body(dari klient) ke kita
+app.use(express.json());
+
 // deafult URl = healtcheck
-app.get("/", (req, res) => {
+app.get("/wahyu", (req, res) => {
+  res.status(200).json({
+    message: "Ping Succesfully!",
+  });
+});
+
+//ditaruh diluar
+const cars = JSON.parse(
+  fs.readFileSync(`${__dirname}/data/cars.json`, "utf-8") // membaca file json harus ada utf-8
+);
+
+// /api/v1/(collectionnya cars) => collectionnya ini  harus jamak (s)
+app.get("/api/v1/cars", (req, res) => {
   res.status(200).json({
     status: "success",
-    message: "Aplikasi ini sudah berjalan dengan baik brookkk",
+    message: "succes get data brok",
+    isSuccess: true,
+    totalData: cars.length, // untuk mengetahui jumlah data
+    data: {
+      cars,
+    },
+  });
+});
+
+// respon.data.cars
+
+app.post("/api/v1/cars", (req, res) => {
+  // insert into menggunakan ORM
+
+  //proses 1
+  const newCar = req.body; // ini adalah sebuah object
+
+  cars.push(newCar);
+
+  // proses write ke  d alam datbasenya
+  //strungify untuk mengubah object menjadi string
+  fs.writeFileSync(`${__dirname}/data/cars.json`, JSON.stringify(cars), (err) => {     // menulis data ke dalam file json langsung saja taruh data cars nya menggunakan callback
+    res.status(201).json({
+      status: "success",
+      message: "succes get data brok!!!",
+      isSuccess: true,
+      totalData: cars.length,
+      data: {
+        car: newCar,
+      },
+    });
+  }) 
+
+
+  // const cars = JSON.parse(
+  //   fs.readFileSync(`${__dirname}/data/cars.json`, "utf-8")
+  // );
+
+  res.status(200).json({
+    status: "success",
+    message: "succes get data brok",
+    isSuccess: true,
+    data: cars,
   });
 });
 
