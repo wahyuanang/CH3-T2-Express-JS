4f07b0c9f4a0b96cd6514bffe0a322e3a46ac7fa
wahyuanang <wahyuanng@students.amikom.ac.id>
2024-10-03 21:15:28 +0700

    add metode get by id in API

--- server.js
+++ server.js
@@ -41,14 +41,12 @@ app.get("/api/v1/cars", (req, res) => {
 
 // respon.data.cars
 
+// untuk post data
 app.post("/api/v1/cars", (req, res) => {
   // insert into menggunakan ORM
-
   //proses 1
   const newCar = req.body; // ini adalah sebuah object
-
   cars.push(newCar);
-
   // proses write ke  d alam datbasenya
   //strungify untuk mengubah object menjadi string
   fs.writeFileSync(
@@ -67,11 +65,9 @@ app.post("/api/v1/cars", (req, res) => {
       });
     }
   );
-
   // const cars = JSON.parse(
   //   fs.readFileSync(`${__dirname}/data/cars.json`, "utf-8")
   // );
-
   res.status(200).json({
     status: "success",
     message: "succes get data brok",
@@ -80,9 +76,30 @@ app.post("/api/v1/cars", (req, res) => {
   });
 });
 
+// jangan lupa untuk mengganti get post ataau yang lainya
+// untuk get data berdasarkan id
+app.get("/api/v1/cars/:id", (req, res) => {
+  // select * from FSW2 where id = "1" or namae = 'wahyu' jika menggunakan row query
+  const id = req.params.id;
+  console.log(id);
+
+  // pelajari maksud dibawah ini
+  // bedanya === dan == adalah === itu strict maka akan sangat sensitif dengan tipe data meskipun, sedangkan == itu tidak strict dia tdak akan memperhatikan tipe data meskipun dia itu string atau number
+  const car = cars.find((i) => i.id === id);
+
+  // menulis data ke dalam file json langsung saja taruh data cars nya menggunakan callback
+  return res.status(201).json({
+    status: "success",
+    message: "succes get data brok!!!",
+    isSuccess: true,
+    data: {
+      car,
+    },
+  });
+});
+
 //   middleware / handler untuk url yang tidak dapar diakses, soalnya dia tidak ada di aplikasi
 //   ada option utnuk membuat middleware sendiri atau menggunakan middleware bawaan express
-
 app.use((req, res, next) => {
   res.status(404).json({
     status: "error",
